8564bf0d147ea4b0ae5072155f295161fdf2235a
Hongxiang Chen <we-taper@users.noreply.github.com>
2020-01-30 23:11:49 +0000

    Fix a typo

--- setup_env.sh
+++ setup_env.sh
@@ -3,5 +3,5 @@ source ~/miniconda3/bin/activate
 conda activate puddletag
 conda install --yes -n puddletag -c anaconda pyqt=4 configobj
 conda install --yes -n puddletag pyparsing
-conda install --yes -n puddletag-c conda-forge mutagen
+conda install --yes -n puddletag -c conda-forge mutagen
 git clone git@github.com:keithgg/puddletag.git
